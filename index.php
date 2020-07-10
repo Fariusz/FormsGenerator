39144d8845d1ab785926c11ebc2c0f1188f37ca0
Fariusz <r.loth@icloud.com>
2020-07-10 09:51:46 +0100

    Zmieniono stronę główną

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
- 
+
     session_start();
-     
+
     if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true)) //jeżeli jestemśmy zalogowanie to nastąpi przekierowanie do strony strona_glowna.php
     {
-        header('Location: strona_glowna.php'); //przekierowanie
+        header('Location: strona_glowna.html'); //przekierowanie
         exit(); //zatrzymanie wykonywania dalszej części kodu jeżeli warunek jest prawdziwy
     }
- 
+
 ?>
 <!DOCTYPE HTML>
 <html lang="pl">
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="style.css" />
     <script src="script.js"></script>
 </head>
- 
+
 <body>
     <input type="button" value="Login" name="logowanie" onclick=window.location.href='login.php'; /> <!--przekierowanie do nowego okna -->
 	<center>
@@ -27,7 +27,7 @@
 			<h3 name="tresc">Proszę się zalogować</h3>
 		</div>
 	</center>
-	
+
 	<footer>
 		wersja strony: 1.0.1
 	</footer>
--- zaloguj.php
+++ zaloguj.php
@@ -1,19 +1,19 @@
 <?php
 
 	session_start(); //start sesji
-	
+
 	if ((!isset($_POST['login'])) || (!isset($_POST['haslo']))) //jeżeli się nie zalogowano to następuje przekierowanie do index.php
 	{
 		header('Location: index.php'); //przekierowanie do index.php
 		exit(); //przekierowanie wykonywania dalszego kodu
 	}
 
-	require_once "connect.php"; //wklejenie zawartości pliku connect.php to to miejsce + słowo _once sprawdza czy plik nie został 
-								//już wcześniej użyty w kodzie programu a funckji require wymaga pliku w kodzie czy jeżeli brak 
+	require_once "connect.php"; //wklejenie zawartości pliku connect.php to to miejsce + słowo _once sprawdza czy plik nie został
+								//już wcześniej użyty w kodzie programu a funckji require wymaga pliku w kodzie czy jeżeli brak
 								//pliku dalsza czść kodu się nie wykona
 
 	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name); //zmienna + zebranie zmiennych w jednym miejscu
-	
+
 	if ($polaczenie->connect_errno!=0) //jeżeli nie udało się połaczyć pokaże się błąd
 	{
 		echo "Error: ".$polaczenie->connect_errno;
@@ -22,10 +22,10 @@
 	{
 		$login = $_POST['login'];
 		$haslo = $_POST['haslo'];
-		
+
 		$login = htmlentities($login, ENT_QUOTES, "UTF-8"); //metoda ENT_QUOTES sprawia że zabezpieczyliśmy się przed wstrzyknięciem SQLa do zapytania
 		$haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8"); //czyli ciężej będzie teraz włamać się do naszej bazy danych
-	
+
 		if ($rezultat = @$polaczenie->query(
 		sprintf("SELECT * FROM użytkownik WHERE login='%s' AND hasło='%s'",
 		mysqli_real_escape_string($polaczenie,$login),
@@ -35,7 +35,7 @@
 			if($ilu_userow>0)
 			{
 				$_SESSION['zalogowany'] = true; //ustawiliśmy sobie tu flage
-				
+
 				$wiersz = $rezultat->fetch_assoc();
 				$_SESSION['id'] = $wiersz['id_u'];
 				$_SESSION['nazwa_uz'] = $wiersz['nazwa_użytkownika'];
@@ -44,18 +44,18 @@
 
 				unset($_SESSION['blad']);
 				$rezultat->free_result();
-				header('Location: strona_glowna.php');
-				
+				header('Location: strona_glowna.html');
+
 			} else {
-				
+
 				$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
 				header('Location: index.php');
-				
+
 			}
-			
+
 		}
-		
+
 		$polaczenie->close(); //zamknięcie połączenia
 	}
-	
+
 ?>
